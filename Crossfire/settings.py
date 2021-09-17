c153a5983da444cfb859a3c5517febf509c3829d
Pjotr <DevPjotr@tutanota.com>
2021-09-17 21:50:16 +0200

    :recycle: Reformatted code

--- Crossfire/settings.py
+++ Crossfire/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pages.apps.PagesConfig",
-    "forum.apps.ForumConfig"
+    "forum.apps.ForumConfig",
 ]
 
 MIDDLEWARE = [
